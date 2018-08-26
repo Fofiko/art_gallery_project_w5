4a7c83d703afc96d19c9ac548f0042ebfca769ff
Fofiko <foteinid@gmail>
2018-08-26 19:02:52 +0100

    Add sql and runner

---
+++ db/art_gallery.sql
@@ -0,0 +1,14 @@
+DROP TABLE artists;
+DROP TABLE exhibits;
+
+CREATE TABLE artists (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE exhibits (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  artist_id INT4 REFERENCES artists(id)
+);
