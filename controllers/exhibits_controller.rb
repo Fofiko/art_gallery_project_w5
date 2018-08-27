77e1e1d3e60b999e51a82496bdeb255194258174
Fofiko <foteinid@gmail>
2018-08-27 11:21:07 +0100

    Add exhibits controller and index, show ERBs

---
+++ controllers/exhibits_controller.rb
@@ -0,0 +1,14 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require_relative("../models/exhibit.rb")
+also_reload("../models/*")
+
+get '/exhibits' do
+  @exhibits = Exhibit.all()
+  erb (:"exhibits/index")
+end
+
+get '/exhibits/:id' do
+  @exhibit = Exhibit.find(params['id'].to_i)
+  erb(:"exhibits/show")
+end
