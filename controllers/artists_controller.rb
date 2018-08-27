59a2407eb5093fcbc8d57ab1c6e46bfe3b78c6ab
Fofiko <foteinid@gmail>
2018-08-27 12:19:43 +0100

    Add new artist action to controller and include matching erb

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -3,11 +3,19 @@ require("sinatra/contrib/all")
 require_relative("../models/artist.rb")
 also_reload("../models/*")
 
+
+#index
 get '/artists' do
   @artists = Artist.all()
   erb (:"artists/index")
 end
 
+#new
+get '/manager/artists/new' do
+  erb(:"artists/new")
+end
+
+#show
 get '/artists/:id' do
   @artist = Artist.find(params['id'].to_i)
   erb(:"artists/show")
