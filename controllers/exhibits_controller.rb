4880aa951731cf8b1b7db09ebe25f7814c6e7b08
Fofiko <foteinid@gmail>
2018-08-27 16:28:29 +0100

    Add new exhibit action to controller with matching erb

--- controllers/exhibits_controller.rb
+++ controllers/exhibits_controller.rb
@@ -1,13 +1,22 @@
 require("sinatra")
 require("sinatra/contrib/all")
 require_relative("../models/exhibit.rb")
+require_relative("../models/artist.rb")
 also_reload("../models/*")
 
+#index
 get '/exhibits' do
   @exhibits = Exhibit.all()
   erb (:"exhibits/index")
 end
 
+#new
+get '/exhibits/new' do
+  @artists = Artist.all()
+  erb(:"exhibits/new")
+end
+
+#show
 get '/exhibits/:id' do
   @exhibit = Exhibit.find(params['id'].to_i)
   erb(:"exhibits/show")
