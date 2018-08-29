e89b9d8af7c217ffa9816a08c34a8b573b4fcb24
Fofiko <foteinid@gmail>
2018-08-29 17:19:18 +0100

    Add visitor_index to main views

--- app.rb
+++ app.rb
@@ -3,10 +3,14 @@ require("sinatra/contrib/all")
 require_relative("controllers/artists_controller")
 require_relative("controllers/exhibits_controller")
 
-get '/' do
-  erb( :index )
+get "/" do
+  erb(:index)
 end
 
-get '/manager' do
-  erb( :manager_index )
+get "/manager" do
+  erb(:manager_index)
+end
+
+get "/visitor" do
+  erb(:visitor_index)
 end
